9f27afcf612605bdf940a9b6a4a49ad0d08e1c37
Babak Akbarzadeh <Babak.akbarzadeh@ugent.be>
2024-10-03 14:53:47 +0200

    logistic regression

--- LinearRegression/Test.py
+++ LinearRegression/Test.py
@@ -19,24 +19,3 @@ model.trainModel()
 modelMulti = MultiVarModel(mtcars[['wt','qsec']], mtcars['mpg'], True)
 modelMulti.trainModel()
 print(modelMulti.weight)
-
-
-from sklearn import linear_model
-
-# Initialize model
-regression_model = linear_model.LinearRegression()
-
-# Train the model using the mtcars data
-regression_model.fit(X = pd.DataFrame(mtcars[['wt','qsec']]), 
-                     y = mtcars["mpg"])
-
-# Check trained model y-intercept
-print(regression_model.intercept_)
-
-# Check trained model coefficients
-print(regression_model.coef_)
-
-
-print(modelMulti.calculateMSE(modelMulti.weight, modelMulti.bias))
-
-print(modelMulti.calculateMSE(regression_model.coef_ , regression_model.intercept_))
